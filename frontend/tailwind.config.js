bb6ad92cae3d5825ae63cc237867a718f6f02617
* <nexuss0781@gmail.com>
2025-07-18 01:38:19 +0300

    Update README with new instructions

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,10 @@
-// This is the object to replace inside tailwind.config.js
-theme: {
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
     extend: {
         colors: {
             'primary': {
@@ -15,4 +20,6 @@ theme: {
             'lg': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(255, 255, 255, 0.05)',
         }
     },
-},
+  },
+  plugins: [],
+}
